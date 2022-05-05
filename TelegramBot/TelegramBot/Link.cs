3c4f5201bf42eb55032935d21d930a708a024e51
Igor <igor.makhotkin@gmail.com>
2022-05-05 18:55:56 +0300

    добавлена модель данных для базы данных

---
+++ TelegramBot/TelegramBot/Link.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TelegramBot
+{
+    // Модель данных для базы данных
+    public class Link
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string? Url { get; set; }
+
+        public string? Category { get; set; }
+    }
+}
