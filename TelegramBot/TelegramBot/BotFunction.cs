f851ca9d44a6df79bbceb56a1e22a2d4c4c679d9
Igor <igor.makhotkin@gmail.com>
2022-05-05 19:02:39 +0300

    Класс с функция бота

---
+++ TelegramBot/TelegramBot/BotFunction.cs
@@ -0,0 +1,33 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+    public class BotFunction : ICommand
+    {
+        public static bool SaveLinksFlag { get; private set; } = false;
+
+        public static bool SaveLinksStage1 { get; private set; } = false;
+
+        public static bool SaveLinksStage2 { get; private set; } = false;
+
+        public static bool GetLinksFlag { get; private set; } = false;
+
+        public static bool GetLinksStage1 { get; private set; } = false;
+
+        public static string tempLinks { get; private set; } = string.Empty;
+
+        public static string tempCategory { get; private set; } = string.Empty;
+
+
+        public static async Task SaveLinks(ITelegramBotClient botClient, Message message)
+        {
+            
+        }
+
+        public static async Task GetLinks(ITelegramBotClient botClient, Message message)
+        {
+           
+        }
+    }
+}
