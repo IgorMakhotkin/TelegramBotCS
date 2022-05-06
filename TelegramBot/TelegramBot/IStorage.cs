4dd6d9d5f6de18ce23edc55f81473536f7760b80
Igor <igor.makhotkin>
2022-05-06 21:35:43 +0300

    Add class Startup and Istorage

---
+++ TelegramBot/TelegramBot/IStorage.cs
@@ -0,0 +1,12 @@
+﻿
+namespace TelegramBot
+{
+    //Интерфейс хранилиша 
+    public interface IStorage
+    {
+        public abstract string ReturnLinks(string key);
+
+        public abstract Task<bool> AddLinksToStorage(string key, string value);
+
+    }
+}
