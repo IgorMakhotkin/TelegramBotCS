15deb69699ffc753a70a3e79cf4c9081033d5801
Igor <igor.makhotkin>
2022-05-12 19:57:02 +0300

    add TelegramCommandInput

---
+++ TelegramBot/TelegramBot/TelegramCommandInput.cs
@@ -0,0 +1,28 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+    public class TelegramCommandInput
+    {
+        public string TextMessage { get; set; }
+
+        public ITelegramBotClient client { get; set; }
+
+        public Message message { get; set; }
+
+        public TelegramCommandInput(ITelegramBotClient botClient, Message receivedMessage, string messageToSend)
+        {
+            this.client = botClient;
+            this.message = receivedMessage;
+            this.TextMessage = messageToSend;
+        }
+
+        public async Task sendMessage()
+        {
+            var chatId = message.Chat.Id;
+            await client.SendTextMessageAsync(chatId, TextMessage);
+        }
+    }
+}
+
