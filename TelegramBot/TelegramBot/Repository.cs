0bfb728bf04ec10af1cb9ff88a4057dc8ec5454e
Igor <igor.makhotkin>
2022-05-13 09:47:38 +0300

    add Repository and IRepository

---
+++ TelegramBot/TelegramBot/Repository.cs
@@ -0,0 +1,73 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+    public class Repository : IRepository
+    {
+        private ICommand Send { get; set; }
+
+        private ITelegramBotClient Client { get; set; }
+
+        private Message Message { get; set; }
+
+        public Repository(ITelegramBotClient botClient, Message receivedMessage, ICommand send)
+        {
+            this.Client = botClient;
+            this.Message = receivedMessage;
+            this.Send = send;
+        }
+
+        public async Task ExecutAsync()
+        {
+            if (BotFunction.SaveLinksFlag)
+            {
+                await this.StoreLink(Client, Message, Send);
+                return;
+            }
+
+            if (BotFunction.GetLinksFlag)
+            {
+                await this.GetLink(Client, Message, Send);
+                return;
+            }
+            else
+            {
+                await(Message.Text!.Split(' ')[0] switch
+                {
+
+                    "/start" => this.Usage(Client, Message, Send),
+                    "/store_link" => this.StoreLink(Client, Message, Send),
+                    "/get_links" => this.GetLink(Client, Message, Send),
+
+                });
+            }
+        }
+
+        public async Task Usage(ITelegramBotClient botClient, Message message, ICommand send)
+        {
+            const string usage = "Функции:\n" +
+                                 "/store_link   - записать ссылку\n" +
+                                 "/get_links - получить ссылку\n";
+            TelegramCommandInput textInput = new TelegramCommandInput(botClient, message, usage);
+            await send.ExecuteAsync(textInput);
+            return;
+        }
+
+        public async Task StoreLink(ITelegramBotClient botClient, Message message, ICommand send)
+        {
+            string answer = await BotFunction.SaveLinks(message.Text!);
+            TelegramCommandInput textInput = new TelegramCommandInput(botClient, message, answer);
+            await send.ExecuteAsync(textInput);
+            return;
+        }
+
+        public async Task GetLink(ITelegramBotClient botClient, Message message, ICommand send)
+        {
+            string answer = BotFunction.GetLinks(message.Text!);
+            TelegramCommandInput textInput = new TelegramCommandInput(botClient, message, answer);
+            await send.ExecuteAsync(textInput);
+            return;
+        }
+    }
+}
---
+++ TelegramBot/TelegramBot/interface/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot
+{
+    public interface IRepository
+{
+        public Task Usage(ITelegramBotClient botClient, Message message, ICommand send);
+
+        public Task StoreLink(ITelegramBotClient botClient, Message message, ICommand send);
+
+        public Task GetLink(ITelegramBotClient botClient, Message message, ICommand send);
+
+        public Task ExecutAsync();
+
+}
+}
