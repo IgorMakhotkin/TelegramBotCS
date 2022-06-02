2663081e6e8ef21fd72fbe533e7f600322da2b7b
Igor <igor.makhotkin@gmail.com>
2022-06-02 23:51:08 +0300

    Add data base context

---
+++ WebPortal/WebPortal/db/Link.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace WebApplication1.db
+{
+    public class Link
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public long UserId { get; set; }
+
+        public string? Url { get; set; }
+
+        public string? Category { get; set; }
+    }
+}
