d04c9ff532fe25489ef58cd85f5a9cce9179d36e
Igor <igor.makhotkin@gmail.com>
2022-06-03 00:12:19 +0300

    Add user authenticated

---
+++ WebPortal/WebPortal/Controllers/AccountController.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebPortal.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebPortal.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            ViewData["ReturnUrl"] = model.ReturnUrl;
+            CheckAccount check = new CheckAccount();
+
+            if (check.IsAuthenticated(model))
+            {
+                if(User.Identity.IsAuthenticated)
+                {
+                    Console.WriteLine("User Authenticated");
+                }
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, model.Username)
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(new ClaimsPrincipal(claimsIdentity));
+
+                if (Url.IsLocalUrl(model.ReturnUrl))
+                {
+                    return Redirect(model.ReturnUrl);
+                }
+
+                return RedirectToAction(nameof(HomeController.Index), "UserContent");
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
+        }
+    }
+}
---
+++ WebPortal/WebPortal/db/Link.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+namespace WebPortal.db
+{
+    public class Link
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public long UserId { get; set; }
+
+        public string? Url { get; set; }
+
+        public string? Category { get; set; }
+    }
+}
