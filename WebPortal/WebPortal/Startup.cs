fba2607374f0deca6fe4fce7bb555718e1a1188c
Igor <igor.makhotkin@gmail.com>
2022-06-03 23:29:38 +0300

    Add Logger

---
+++ WebPortal/WebPortal/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using WebPortal.Logger;
+
+namespace WebPortal
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "Logs"));
+        }
+    }
+}
